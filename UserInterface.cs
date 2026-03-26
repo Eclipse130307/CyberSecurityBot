babb46e9fe8c1a768ab5c7fb819cb0c81ef1546c
Eclipse1303 <tevinp1303@gmail.com>
2026-03-26 20:37:44 +0200

    Implemented user Interface and welcome display

---
+++ UserInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurityBot
+{
+    public static class UserInterface
+    {
+        public static void DisplayAsciiArt()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine(@"
+    ========================================
+       CYBERSECURITY AWARENESS CHATBOT
+    ========================================"
+        );
+
+            Console.ResetColor();
+        }
+
+        public static string GetUserName()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(name))
+            {
+                Console.Write("Name cannot be empty. Try again: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome, {name}!");
+            return name;
+        }
+    }
+}
