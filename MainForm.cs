8a7895589dccab2793bd26732a01f6d1f0b397a7
Eclipse1303 <tevinp1303@gmail.com>
2026-05-12 15:22:19 +0200

    Added chatbot GUI interaction logic

---
+++ MainForm.cs
@@ -0,0 +1,47 @@
+﻿using CyberSecurityBot;
+using System;
+using System.Windows.Forms;
+
+namespace CyberSecurityChatbotGUI
+{
+    public partial class MainForm : Form
+    {
+        // Creates chatbot object
+        private Chatbot chatbot = new Chatbot();
+
+        public MainForm()
+        {
+            // Loads all GUI controls
+            InitializeComponent();
+
+            // Plays greeting audio
+            AudioPlayer.PlayGreeting();
+
+            // Welcome message
+            rtbChat.AppendText("Bot: Welcome to the Cybersecurity Awareness Chatbot.\n");
+            rtbChat.AppendText("Bot: Ask me about passwords, phishing, scams, privacy, or safe browsing.\n\n");
+        }
+
+        // Runs when Send button is clicked
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // Gets user message
+            string userInput = txtUserInput.Text;
+
+            // Displays user message
+            rtbChat.AppendText("You: " + userInput + "\n");
+
+            // Gets chatbot response
+            string response = chatbot.GetResponse(userInput);
+
+            // Displays chatbot response
+            rtbChat.AppendText("Bot: " + response + "\n\n");
+
+            // Clears textbox
+            txtUserInput.Clear();
+
+            // Returns cursor to textbox
+            txtUserInput.Focus();
+        }
+    }
+}
